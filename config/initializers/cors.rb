44326fa02509282c18f337f02e7a18669b7055da
Fernanda de Jesus <fernandabussular@gmail.com>
2023-12-02 01:56:34 -0300

    minimal structure

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins 'http://api-synonyms.fly.dev/'
 
-    resource "*",
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
