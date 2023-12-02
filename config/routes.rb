83ce466ae229f1493329b98623583e8827efd385
Fernanda de Jesus <fernandabussular@gmail.com>
2023-12-02 20:03:53 -0300

    minimal structure

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post '/auth/login', to: 'authentication#login'
       root to: 'words#index'
-      resources :words, except: :index do
+      resources :words do
         collection do
           post 'create_synonym_and_word'
           get 'unreviewed_synonyms'
