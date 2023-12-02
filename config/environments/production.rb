57689a23f2c5625aeac6a57b13403f46cce53d25
Fernanda de Jesus <fernandabussular@gmail.com>
2023-12-02 18:44:05 -0300

    minimal structure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -84,6 +86,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.secret_key_base = Rails.application.credentials.secret_key_base
 end
