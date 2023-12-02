339d5d066d97fd67d6ac26e8ec778d99fdc9731b
Fernanda de Jesus <fernandabussular@gmail.com>
2023-12-02 20:28:08 -0300

    minimal structure

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,12 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       post '/auth/login', to: 'authentication#login'
-      root '/api-docs/index.html', to: 'words#index'
+      root to: 'words#index'
       resources :words do
-        collection do
-          post 'create_synonym_and_word'
-          get 'unreviewed_synonyms'
-          put 'authorize_synonym'
-          delete 'delete_synonym'
-        end
+        post 'create_synonym_and_word'
+        get 'unreviewed_synonyms'
+        put 'authorize_synonym'
+        delete 'delete_synonym'
       end
     end
   end
